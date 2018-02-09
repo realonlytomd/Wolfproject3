75f045cec4e0c3cb26d7932dc0a50aa3e58e842f
realonlytomd <mclaughlintomd@gmail.com>
2018-02-09 13:32:45 -0600

    added dummy index file to practice branching

---
+++ index.js
@@ -0,0 +1,15 @@
+// this is a dummy file for us to practice our github flow
+//
+// After creating your own branch from the project 3 repo, add your name in 
+// a comment line below the rest, and push it to your branch in the repo.
+// then initiate a pull request
+//
+    // Tom McLaughlin
+//
+// this file was an index.js file from the class repo, Chapter 18 - Mongo, Activity 20
+
+module.exports = {
+    Article: require("./Article"),
+    Note: require("./Note")
+  };
+  
