fe4655c894ae0fc9501159554294ce6661530038
realonlytomd <mclaughlintomd@gmail.com>
2018-03-04 15:59:40 -0600

    incorporate css changes

--- client/src/pages/Chores/Chores.js
+++ client/src/pages/Chores/Chores.js
@@ -57,28 +57,28 @@ class Chores extends Component {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-6">
+          <Col size="md-5">
             <Jumbotron>
-              <h1>What Chores Should I Do?</h1>
+              <h1>Create a Chore</h1>
             </Jumbotron>
             <form>
               <Input
                 value={this.state.title}
                 onChange={this.handleInputChange}
                 name="title"
-                placeholder="Chore Name (required)"
+                placeholder="Name of Chore (required)"
               />
               <Input
                 value={this.state.kid}
                 onChange={this.handleInputChange}
                 name="kid"
-                placeholder="Kid (required)"
+                placeholder="Child (required)"
               />
               <TextArea
                 value={this.state.reward}
                 onChange={this.handleInputChange}
                 name="reward"
-                placeholder="Reward (Optional)"
+                placeholder="Reward (Points or Tangible)"
               />
               <FormBtn
                 disabled={!(this.state.kid && this.state.title)}
@@ -88,9 +88,9 @@ class Chores extends Component {
               </FormBtn>
             </form>
           </Col>
-          <Col size="md-6 sm-12">
+          <Col size="md-7 sm-12">
             <Jumbotron>
-              <h1>Chores On My List</h1>
+              <h1>Chores to Complete</h1>
             </Jumbotron>
             {this.state.chores.length ? (
               <List>
@@ -98,7 +98,7 @@ class Chores extends Component {
                   <ListItem key={chore._id}>
                     <Link to={"/chores/" + chore._id}>
                       <strong>
-                        {chore.title} by {chore.kid}
+                        {chore.kid}: {chore.title}
                       </strong>
                     </Link>
                     <DeleteBtn onClick={() => this.deleteChore(chore._id)} />
@@ -106,7 +106,7 @@ class Chores extends Component {
                 ))}
               </List>
             ) : (
-              <h3>No Results to Display</h3>
+              <h3>No Chores to Do</h3>
             )}
           </Col>
         </Row>
