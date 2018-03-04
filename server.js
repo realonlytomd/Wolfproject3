eddfac22171efaaac48ed71af9954d06e55bd7f0
tmac4403 <drive911@gmail.com>
2018-03-04 10:05:29 -0600

    sunday push

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 const PORT = process.env.PORT || 3001;
-
 // Configure body parser for AJAX requests
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use(require("express-session")({
-  secret:"random strings here are good",
+  secret: "random strings here are good",
   resave: false,
   saveUninitialized: false
 }));
@@ -56,12 +57,13 @@ mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  // should I put another db?
   {
     useMongoClient: true
   }
 );
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
