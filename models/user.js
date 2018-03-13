5de353dfd19a2712ffb5c31cf81ac5b0d0c05b0e
realonlytomd <mclaughlintomd@gmail.com>
2018-03-12 21:23:20 -0500

    change user.js back to tom

--- models/user.js
+++ models/user.js
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
 
-// const passportLocalMongoose = require("passport-local-mongoose")
-// const User = new Schema({
-//     username: { type: String, required: true },
-//     password: { type: String, required: true }
-//     // firstName: String,
-//     // lastName: String
-// });
-// User.plugin(passportLocalMongoose);
-// module.exports = mongoose.model("User", User);
-
-// const User = mongoose.model("User", userSchema);
-
-// module.exports = User;
-=======
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -28,4 +11,4 @@ const userSchema = new Schema({
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
->>>>>>> 4513b82c1015814854aad05d38d27249bb327ce1
+
